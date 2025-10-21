2c6f12c3695caa560601dc0f2df5103f7379f7a5
Code Classroom <benjamin.underdahl@proton.me>
2025-10-20 23:40:25 -0500

    created functions for platform specific pickers

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './coin_data.dart';
 import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   
@@ -9,6 +10,38 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  DropdownButton<String> androidPicker() {
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: [
+        for (String currency in currenciesList)
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency
+        ),
+      ],
+      onChanged: (String? value) {
+        setState(() {
+          selectedCurrency = value!;
+        }
+        );
+      }, 
+    );
+  }
+
+  CupertinoPicker iosPicker() {
+    return CupertinoPicker(
+      itemExtent: 32, 
+      onSelectedItemChanged: (selectedIndex) {
+        selectedCurrency = currenciesList[selectedIndex];
+      }, 
+      children: [
+        for (String currency in currenciesList)
+        Text(currency)
+      ]);
+  }
+
   String selectedCurrency = "USD";
   @override
   Widget build(BuildContext context) {
@@ -46,26 +79,10 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: CupertinoPicker(
-              backgroundColor: Colors.lightBlue,
-              itemExtent: 32, 
-              onSelectedItemChanged: (selectedIndex) {
-                selectedCurrency = currenciesList[selectedIndex];
-              }, 
-              children: [
-                for(String currency in currenciesList)
-                Text(currency)
-              ]
-              ), 
+            child: Platform.isIOS ? iosPicker() : androidPicker()
             ),
         ],
       ),
     );
   }
 }
-
-// for (String currency in currenciesList)
-//                 DropdownMenuItem(
-//                 child: Text(currency),
-//                 value: currency,
-//                 ),
